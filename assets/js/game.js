f8f84a4267496ac0009b9283e5b10cc1af765420
joehenrickson <joehenrickson@gmail.com>
2022-04-01 22:36:51 -0500

    fights all robots until all die

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,8 +15,9 @@ var enemyAttack = 12;
 
 // fight function 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+  // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
+    // place fight function code block here . . .
 
   // ask player if they'd like to fight or skip  
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -66,10 +67,13 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
    // if player did not chose 1 or 2 in prompt
     } else {
     window.alert("You need to pick a valid option. Try again!");
+    }
   }
 };
 
-// run fight function to start game
-for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+// call fight function with enemy-robot
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
